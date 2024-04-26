e4a8383a42afeed0a9a2ef23d1562b2e18ea985a
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-04-26 14:22:58 +0300

    Add files via upload

---
+++ кп 2 задача 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // целые
+            Triangle<int> triInt = new Triangle<int>();
+            Console.WriteLine($"Целочисленный треугольник: ");
+            Console.WriteLine($"Площадь: {triInt.Area(5, 4, 3)}");
+
+            // беззнаковые целые
+            Triangle<uint> triUint = new Triangle<uint>();
+            Console.WriteLine($"\nБеззнаковый целочисленный треугольник: ");
+            Console.WriteLine($"Площадь: {triUint.Area(11, 21, 31)}");
+
+            // вещественные
+            Triangle<double> triDouble = new Triangle<double>();
+            Console.WriteLine($"\nВещественный треугольник: ");
+            Console.WriteLine($"Площадь: {triDouble.Area(1.2, 1.8, 3.0)}");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ кп 2 задача 1/Triangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Triangle<T>
+    {
+
+        public T a { get; set; }
+
+        public T b { get; set; }
+
+        public T c { get; set; }
+        
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Triangle()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public Triangle( T a, T b, T c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        /// <summary>
+        /// Метод вывода информации о сторонах треугольника
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.Write($"Сторона a = {a}\nСторона b ={b}\nСторона c = {c}");
+        }
+
+
+        public double Area(double a, double b, double c)
+        {
+            double perimetr = a + b + c;
+            double polyperimetr = (perimetr / 2);
+            return Math.Sqrt(polyperimetr * (polyperimetr - a) * (polyperimetr - b) * (polyperimetr - c));
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
